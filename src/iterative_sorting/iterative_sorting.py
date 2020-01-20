a5b00d3838d89a1bcbd4a4e0bd14255d4a9f7429
leslie <rleslie1015@gmail.com>
2020-01-20 00:10:17 -0600

    why do my inner and outer loops have the same index?

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,23 +4,24 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i #i guess this is a temp var
         smallest_index = cur_index
+        print(f'current index in outer loop {cur_index}')
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-        for cur_index in range(0, len(arr) - 1):
-            while cur_index > 0 and cur_index < i:
-                if cur_index < smallest_index:
-                    smallest_index = cur_index
-              
-
-        # TO-DO: swap
-
-
+        
+        #Loop through elements on right-hand-side of current index
+        for j in range(cur_index+1, len(arr)):
+            print(f'current index in innner loop {cur_index}')
+            #find the smallest index
+            if cur_index < j:
+                # Swap the element at current index with the smallest element found in above loop
+                j = smallest_index
+                print(j)
 
 
     return arr
 
-items = [2,4,5,6,8,1,3,7,9]
+items = [9,4,5,6,8,1,3,7,2]
 selection_sort(items)
+print(items)
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
