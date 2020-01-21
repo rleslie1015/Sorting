cb08a275bb99f6dd5f7d62739c122b00b409bc1f
leslie <rleslie1015@gmail.com>
2020-01-21 16:44:33 -0600

    iterative ok

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,26 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
+
     for i in range(0, len(arr) - 1):
-        cur_index = i 
+        # cur_index = i 
         smallest_index = cur_index
+      
         # TO-DO: find next smallest element
         #Loop through elements on right-hand-side of current index
         for j in range(i+1, len(arr)):
-            a = arr[j]
-            #find the smallest index
-            if a < cur_index:
-                smallest_index = a
-        # Swap the element at current index with the smallest element found in above loop
-        if(i != smallest_index):
-            arr[i] = arr[smallest_index]
-            arr[smallest_index] = arr[i]
+         
             
-
-
+            #find the smallest index
+            if arr[j] < arr[smallest_index]:
+                #swap with smallest
+                smallest_index = j
+        # Swap the element at current index with the smallest element found in above loop
+        if smallest_index != i:
+            arr[smallest_index], arr[i] = arr[i], arr[smallest_index]
     return arr
 
-items = [9,4,5,6,8,1,3,7,2]
+items = [5,4,3,2,1]
 selection_sort(items)
 print(items)
 
@@ -28,25 +28,21 @@ print(items)
 # Bubble sort : Basically just goes throuht the list and compares two items and swaps those two items in order.
 # The loop stops after a list has no more items to swap.
 def bubble_sort( arr ):
-    for i in range(0, len(arr)):
-        print(arr[i+1])
-        print(arr[i])
-        if arr[i] < arr[i+1]:
-            smallest = arr[i]
-            arr[i] = arr[i+1]
-            print(arr[i])
-
-
-
     # for i in range(0, len(arr)):
-
-
-
+    for i in range(0, len(arr) -1):
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
     return arr
+
+
+
 list2 = [5,4,3,2,1]
 bubble_sort(list2)
-print(list2)
-# STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
 
-    return arr
+# STRETCH: implement the Count Sort function below
+# def count_sort( arr, maximum=-1 ):
+
+#     return arr
